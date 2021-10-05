e83bc6f8782ea3d8ccf6748d4d064f3512d687ce
mparulski <michal.parulski@gmail.com>
2021-10-05 22:12:45 +0200

    project configuration

--- __config/webpack.prod.config.js
+++ __config/webpack.prod.config.js
@@ -2,7 +2,10 @@ const path = require('path')
 
 module.exports = env => {
   return {
-    entry: {index: './src/index.ts'},
+    entry: {
+      getCase: './src/getCase.ts',
+      isEmptyFunction: './src/isEmptyFunction.ts'
+    },
     output: {
       path: path.resolve(__dirname, '../dist'),
       filename: '[name].js',
